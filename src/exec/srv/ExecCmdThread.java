7ddfcde59abf69643c6bbfd3ea6d966e7a21530b
huangyq <19123767@qq.com>
2014-05-22 14:08:11 +0800

    dev

---
+++ src/exec/srv/ExecCmdThread.java
@@ -0,0 +1,186 @@
+package exec.srv;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+import org.apache.mina.core.session.IoSession;
+
+import exec.common.Command;
+import exec.common.ISmsObject;
+import exec.proto.SmsObjectC102;
+import exec.proto.SmsObjectC104;
+import exec.proto.SmsObjectS102;
+
+public class ExecCmdThread extends Thread {
+	
+	private ExecUser user = null;
+	private ISmsObject isms = null;
+	
+	public ExecCmdThread(ExecUser user, ISmsObject sms) {
+		this.isms = sms;
+		this.user = user;
+	}
+	
+	public void run() {
+		try {
+			String fileName = "";
+			String charsetName = "UTF-8";
+			Process proc = null;
+			if (ExecService.getInstance().getOs() == ExecService.os_unix) {
+				fileName = "_remote_script_" + user.getName() + "_tmp.sh";
+				charsetName = "UTF-8";
+				makeScript(fileName);
+				proc = Runtime.getRuntime().exec("sh " + fileName);
+			} else {
+				fileName = "_remote_script_" + user.getName() + "_tmp.bat";
+				charsetName = "GB2312";
+				makeScript(fileName);
+				proc = Runtime.getRuntime().exec("cmd /c " + fileName);
+			}
+			InputStream isinfo = proc.getInputStream();
+			InputStream iserr = proc.getErrorStream();
+	        InputStreamReader isr = new InputStreamReader(isinfo, charsetName);
+	        BufferedReader br = new BufferedReader(isr);
+	        
+	        InputStreamReader isrerr = new InputStreamReader(iserr, charsetName);
+	        BufferedReader brerr = new BufferedReader(isrerr);
+	        String line = null; 
+	        SmsObjectS102 sms = null;
+	        IoSession session = isms.getSession();
+	        while ((line = br.readLine()) != null){
+	        	sms = new SmsObjectS102();
+	        	sms.setLine(line);
+	        	session.write(sms);
+	        }
+	        int exitVal = proc.waitFor();
+	        while ((line = brerr.readLine()) != null){
+	        	exitVal = 1;
+	        	sms = new SmsObjectS102();
+	        	sms.setLine("ERROR:" + line);
+	        	session.write(sms);
+	        }
+	        if(exitVal == 0){
+	        	sms = new SmsObjectS102();
+	        	sms.setLine("执行成功");
+	        	session.write(sms);
+	        }else{
+	        	sms = new SmsObjectS102();
+	        	sms.setLine("执行失败");
+	        	session.write(sms);
+	        } 
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			ExecService.getInstance().removeThread(user.getName());
+		}
+	}
+	
+	private void makeScript(String fileName) {
+		try {
+			switch (ExecService.getInstance().getOs()) {
+				case ExecService.os_win :
+					makeWinScript(fileName);
+					break;
+				default:
+					makeUnixScript(fileName);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void makeWinScript(String fileName) throws IOException {
+		File file = new File(fileName);
+		OutputStreamWriter write = new OutputStreamWriter(new FileOutputStream(file),"GB2312");
+		BufferedWriter confBf =new BufferedWriter(write);
+		confBf.write("@echo off\r\n");
+		confBf.write("echo 正在执行\r\n");
+		if (isms instanceof SmsObjectC102) {
+			SmsObjectC102 sms102 = (SmsObjectC102)isms;
+			List<String> keyList = sms102.getCmdList();
+			List<Command> cmdList = ExecService.getInstance().getCommandByUser(user);
+			for (String key : keyList) {
+				Command cmd = getCommandByKey(cmdList, key);
+				if (cmd != null) {
+					confBf.write("\r\n");
+					if (cmd.getDir().trim().length() > 0)
+						confBf.write("cd /d " + cmd.getDir() + "\r\n");
+					confBf.write(cmd.getCmd() + "\r\n");
+				}
+			}
+		} else if(isms instanceof SmsObjectC104) {
+			SmsObjectC104 sms104 = (SmsObjectC104)isms;
+			String key = sms104.getCmdkey();
+			List<Command> cmdList = ExecService.getInstance().getCommandByUser(user);
+			Command cmd = getCommandByKey(cmdList, key);
+			if (cmd != null) {
+				confBf.write("\r\n");
+				if (cmd.getDir().trim().length() > 0)
+					confBf.write("cd /d " + cmd.getDir() + "\r\n");
+				confBf.write(replaceParam(cmd.getCmd(), sms104.getParam()) + "\r\n");
+			}
+		}
+		confBf.write("echo finish\n");
+		confBf.close();
+		write.close();
+	}
+	
+	private void makeUnixScript(String fileName) throws IOException {
+		File file = new File(fileName);
+		OutputStreamWriter write = new OutputStreamWriter(new FileOutputStream(file),"GB2312");
+		BufferedWriter confBf =new BufferedWriter(write);
+		confBf.write("#/bin/bash\n");
+		confBf.write("echo 正在执行\r\n");
+		if (isms instanceof SmsObjectC102) {
+			SmsObjectC102 sms102 = (SmsObjectC102)isms;
+			List<String> keyList = sms102.getCmdList();
+			List<Command> cmdList = ExecService.getInstance().getCommandByUser(user);
+			for (String key : keyList) {
+				Command cmd = getCommandByKey(cmdList, key);
+				if (cmd != null) {
+					confBf.write("\n");
+					if (cmd.getDir().trim().length() > 0)
+						confBf.write("cd " + cmd.getDir() + "\r\n");
+					confBf.write(cmd.getCmd() + "\r\n");
+				}
+			}
+		} else if(isms instanceof SmsObjectC104) {
+			SmsObjectC104 sms104 = (SmsObjectC104)isms;
+			String key = sms104.getCmdkey();
+			List<Command> cmdList = ExecService.getInstance().getCommandByUser(user);
+			Command cmd = getCommandByKey(cmdList, key);
+			if (cmd != null) {
+				confBf.write("\n");
+				if (cmd.getDir().trim().length() > 0)
+					confBf.write("cd " + cmd.getDir() + "\r\n");
+				confBf.write(replaceParam(cmd.getCmd(), sms104.getParam()) + "\r\n");
+			}
+		}
+		confBf.write("echo finish\n");
+		confBf.close();
+		write.close();
+	}
+	
+	private String replaceParam(String cmd, String param) {
+		if (param.trim().length() == 0)
+			return cmd;
+		String r = cmd.replaceFirst("\\$\\{param\\}", param);
+		return r;
+	}
+	
+	private Command getCommandByKey(List<Command> cmdList, String key) {
+		for (Command cmd : cmdList) {
+			if (cmd.getKey().equals(key)) {
+				return cmd;
+			}
+		}
+		return null;
+	}
+}
--- src/exec/srv/ExecService.java
+++ src/exec/srv/ExecService.java
@@ -24,18 +24,25 @@ import exec.common.Command;
 import exec.common.ISmsObject;
 import exec.proto.SmsObjectC100;
 import exec.proto.SmsObjectC102;
+import exec.proto.SmsObjectC104;
 import exec.proto.SmsObjectS100;
 import exec.proto.SmsObjectS101;
+import exec.proto.SmsObjectS103;
 
 public class ExecService {
 	
 	private final static Logger log = LoggerFactory.getLogger(ExecService.class);
+	public final static int os_win = 1;
+	public final static int os_unix = 2;
 	
 	private static ExecService instance = null;
 	private HashMap<String, ExecUser> userMap = new HashMap<String, ExecUser>();
-	private List<Command> cmdList = new ArrayList<Command>();
+	private HashMap<String, Thread> threadMap = new HashMap<String, Thread>();
 	private int port = 0;
+	private int os = os_unix;
 	private Map<Class, Method> methodList = new HashMap<Class, Method>();
+	
+	private List<Command> cmdList = new ArrayList<Command>();
 	
 	private ExecService(){
 		Method [] Methods = this.getClass().getMethods();
@@ -58,6 +65,14 @@ public class ExecService {
 		return port;
 	}
 	
+	public int getOs() {
+		return os;
+	}
+	
+	public void removeThread(String key) {
+		threadMap.remove(key);
+	}
+	
 	private void initConfig() {
 		userMap.clear();
 		cmdList.clear();
@@ -77,6 +92,14 @@ public class ExecService {
 		    		Element element = (Element)node;
 			    	if (element.getNodeName().equals("port")) {
 			    		port = Integer.parseInt(element.getAttribute("value").trim());
+			    	}
+			    	if (element.getNodeName().equals("os")) {
+			    		String osstr = element.getAttribute("value").trim();
+			    		if ("win".equalsIgnoreCase(osstr)) {
+			    			os = os_win;
+			    		} else {
+			    			os = os_unix;
+			    		}
 			    	}
 			    	if (element.getNodeName().equals("users")) {
 			    		NodeList userList = element.getChildNodes();
@@ -107,6 +130,7 @@ public class ExecService {
 			    				cmdEle = (Element)cmdNode;
 			    				command.setKey(cmdEle.getAttribute("key").trim());
 			    				command.setGroups(cmdEle.getAttribute("groups").trim());
+			    				command.setUsername(cmdEle.getAttribute("username").trim());
 			    				command.setDesc(cmdEle.getAttribute("desc").trim());
 			    				command.setDir(cmdEle.getAttribute("dir").trim());
 			    				command.setCmd(cmdEle.getTextContent().trim());
@@ -122,6 +146,14 @@ public class ExecService {
 	}
 	
 	public void sessionClosed(IoSession session) {
+		ExecUser user = (ExecUser)session.getAttribute("execUser");
+		if (user != null) {
+			Thread running = threadMap.get(user.getName());
+			if (running != null) {
+				running.interrupt();
+			}
+			threadMap.remove(user.getName());
+		}
 	}
 
 	public ISmsObject handle(SmsObjectC100 obj) {
@@ -143,10 +175,41 @@ public class ExecService {
 	}
 	
 	public ISmsObject handle(SmsObjectC102 sms) {
-		System.out.println("handle 102");
-		List<String> list = sms.getCmdList();
-		for (String cmd : list) {
-			System.out.println("handle 102:" + cmd);
+		ExecUser user = (ExecUser)sms.getSession().getAttribute("execUser");
+		if (user != null) {
+			if (threadMap.get(user.getName()) == null) {
+				ExecCmdThread thread = new ExecCmdThread(user, sms);
+				thread.start();
+				threadMap.put(user.getName(), thread);
+			} else {
+				SmsObjectS103 sms103 = new SmsObjectS103();
+				sms103.setLine("你已经有任务在执行了");
+				sms.getSession().write(sms103);
+			}
+		} else {
+			SmsObjectS103 sms103 = new SmsObjectS103();
+			sms103.setLine("账号信息异常，请重新登录");
+			sms.getSession().write(sms103);
+		}
+		return null;
+	}
+	
+	public ISmsObject handle(SmsObjectC104 sms) {
+		ExecUser user = (ExecUser)sms.getSession().getAttribute("execUser");
+		if (user != null) {
+			if (threadMap.get(user.getName()) == null) {
+				ExecCmdThread thread = new ExecCmdThread(user, sms);
+				thread.start();
+				threadMap.put(user.getName(), thread);
+			} else {
+				SmsObjectS103 sms103 = new SmsObjectS103();
+				sms103.setLine("你已经有任务在执行了");
+				sms.getSession().write(sms103);
+			}
+		} else {
+			SmsObjectS103 sms103 = new SmsObjectS103();
+			sms103.setLine("账号信息异常，请重新登录");
+			sms.getSession().write(sms103);
 		}
 		return null;
 	}
@@ -172,14 +235,16 @@ public class ExecService {
 		}
 	}
 	
-	private List<Command> getCommandByUser(ExecUser user) {
+	public List<Command> getCommandByUser(ExecUser user) {
 		String group = user.getGroup();
+		String username = user.getName();
 		List<Command> list = new ArrayList<Command>();
 		for (Command cmd : cmdList) {
-			if (cmd.getGroups().indexOf(group) != -1) {
+			if (cmd.getGroups().indexOf(group) != -1 || cmd.getUsername().indexOf(username) != -1) {
 				list.add(cmd);
 			}
 		}
 		return list;
 	}
+	
 }
